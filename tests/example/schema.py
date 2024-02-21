708a2aab96cd1581816ccc9c0a5fc0109e674700
Rahul Somani <rsomani95@gmail.com>
2024-02-21 12:48:22 -0500

    - vastly minimize model + schema - use original models instead of proxies (verified that this is still optimized)

--- tests/example/schema.py
+++ tests/example/schema.py
@@ -12,91 +12,19 @@ from query_optimizer import DjangoConnectionField, optimize
 from query_optimizer.filter import DjangoFilterConnectionField
 from query_optimizer.typing import GQLInfo
 
-from .models import (
-    Apartment,
-    Developer,
-    Example,
-    HousingCompany,
-    Owner,
-    PropertyManager,
-)
 from .types import (
     ApartmentNode,
-    ApartmentType,
     BuildingNode,
-    BuildingType,
-    DeveloperType,
-    ExampleType,
     HousingCompanyNode,
-    HousingCompanyType,
-    OwnershipType,
-    OwnerType,
-    People,
-    PostalCodeType,
-    PropertyManagerType,
     RealEstateNode,
-    RealEstateType,
-    SaleType,
 )
 
 
 class Query(graphene.ObjectType):
-    all_postal_codes = DjangoListField(PostalCodeType)
-    all_developers = DjangoListField(DeveloperType)
-    all_property_managers = DjangoListField(PropertyManagerType)
-    all_housing_companies = DjangoListField(HousingCompanyType)
-    all_real_estates = DjangoListField(RealEstateType)
-    all_buildings = DjangoListField(BuildingType)
-    all_apartments = DjangoListField(ApartmentType)
-    all_sales = DjangoListField(SaleType)
-    all_owners = DjangoListField(OwnerType)
-    all_ownerships = DjangoListField(OwnershipType)
-
-    def resolve_all_apartments(parent: None, info: GQLInfo) -> models.QuerySet[Apartment]:
-        return optimize(
-            Apartment.objects.all().annotate(
-                full_address=Concat(
-                    models.F("street_address"),
-                    models.F("floor"),
-                    models.F("apartment_number"),
-                    output_field=models.CharField(),
-                ),
-            ),
-            info,
-        )
-
-    housing_company_by_name = graphene.List(HousingCompanyType, name=graphene.String(required=True))
-
-    def resolve_housing_company_by_name(parent: None, info: GQLInfo, name: str) -> models.QuerySet[HousingCompany]:
-        return optimize(HousingCompany.objects.filter(name=name), info)
-
-    apartment = relay.Node.Field(ApartmentNode)
     paged_apartments = DjangoFilterConnectionField(ApartmentNode)
-    building = relay.Node.Field(BuildingNode)
     paged_buildings = DjangoConnectionField(BuildingNode)
-    real_estate = relay.Node.Field(RealEstateNode)
     paged_real_estates = DjangoConnectionField(RealEstateNode)
-    housing_company = relay.Node.Field(HousingCompanyNode)
     paged_housing_companies = DjangoFilterConnectionField(HousingCompanyNode)
-
-    all_people = graphene.List(People)
-
-    def resolve_all_people(parent: None, info: GQLInfo) -> Iterable[Union[Developer, PropertyManager, Owner]]:
-        developers = optimize(Developer.objects.all(), info)
-        property_managers = optimize(PropertyManager.objects.all(), info)
-        owners = optimize(Owner.objects.all(), info)
-        return itertools.chain(developers, property_managers, owners)
-
-    # --------------------------------------------------------------------
-
-    example = graphene.Field(ExampleType, pk=graphene.Int(required=True))
-    examples = DjangoListField(ExampleType)
-
-    def resolve_example(parent: None, info: GQLInfo, pk: int = None):
-        return Example.objects.get(pk=pk)
-
-    # --------------------------------------------------------------------
-
     debug = graphene.Field(DjangoDebug, name="_debug")
 
 
--- tests/example/types.py
+++ tests/example/types.py
@@ -68,165 +68,6 @@ __all__ = [
     "SaleType",
 ]
 
-
-# Basic
-
-
-class PostalCodeType(DjangoObjectType):
-    class Meta:
-        model = PostalCode
-        fields = [
-            "pk",
-            "code",
-            "housing_companies",
-        ]
-
-
-class DeveloperType(DjangoObjectType):
-    class Meta:
-        model = Developer
-        fields = [
-            "pk",
-            "name",
-            "description",
-            "housing_companies",
-        ]
-
-
-class PropertyManagerType(DjangoObjectType):
-    class Meta:
-        model = PropertyManager
-        fields = [
-            "pk",
-            "name",
-            "email",
-            "housing_companies",
-        ]
-
-
-class HousingCompanyType(DjangoObjectType):
-    class Meta:
-        model = HousingCompany
-        fields = [
-            "pk",
-            "name",
-            "street_address",
-            "postal_code",
-            "city",
-            "developers",
-            "property_manager",
-            "real_estates",
-        ]
-
-    def resolve_name(model: HousingCompany, info: GQLInfo) -> str:
-        return model.name
-
-    greeting = graphene.String()
-    manager = graphene.String()
-    primary = graphene.String()
-
-    @required_fields("name")
-    def resolve_greeting(model: HousingCompany, info: GQLInfo) -> str:
-        return f"Hello {model.name}!"
-
-    @required_fields("property_manager__name")
-    def resolve_manager(model: HousingCompany, info: GQLInfo) -> str:
-        return model.property_manager.name
-
-    @required_fields("real_estates__name")
-    def resolve_primary(model: HousingCompany, info: GQLInfo) -> str:
-        return model.real_estates.first().name
-
-
-class RealEstateType(DjangoObjectType):
-    class Meta:
-        model = RealEstate
-        field = [
-            "pk",
-            "name",
-            "housing_company",
-            "buildings",
-        ]
-
-
-class BuildingType(DjangoObjectType):
-    class Meta:
-        model = Building
-        fields = [
-            "pk",
-            "name",
-            "street_address",
-            "real_estate",
-            "apartments",
-        ]
-
-
-class ApartmentType(DjangoObjectType):
-    class Meta:
-        model = Apartment
-        fields = [
-            "pk",
-            "completion_date",
-            "street_address",
-            "stair",
-            "floor",
-            "apartment_number",
-            "shares_start",
-            "shares_end",
-            "surface_area",
-            "rooms",
-            "building",
-            "sales",
-        ]
-        max_complexity = 10
-
-    @classmethod
-    def filter_queryset(cls, queryset: QuerySet, info: GQLInfo) -> QuerySet:
-        return queryset.filter(rooms__isnull=False)
-
-
-class SaleType(DjangoObjectType):
-    class Meta:
-        model = Sale
-        fields = [
-            "pk",
-            "apartment",
-            "purchase_price",
-            "purchase_date",
-            "ownerships",
-        ]
-
-    @classmethod
-    def filter_queryset(cls, queryset: QuerySet, info: GQLInfo) -> QuerySet:
-        return queryset.filter(purchase_price__gte=1)
-
-
-class OwnerType(DjangoObjectType):
-    class Meta:
-        model = Owner
-        fields = [
-            "pk",
-            "name",
-            "email",
-            "sales",
-            "ownerships",
-        ]
-
-
-class OwnershipType(DjangoObjectType):
-    class Meta:
-        model = Ownership
-        fields = [
-            "pk",
-            "owner",
-            "sale",
-            "percentage",
-        ]
-
-
-# Relay
-
-
 class IsTypeOfProxyPatch:
     @classmethod
     def is_type_of(cls, root, info):
@@ -235,9 +76,9 @@ class IsTypeOfProxyPatch:
         return super().is_type_of(root, info)
 
 
-class ApartmentNode(IsTypeOfProxyPatch, DjangoObjectType):
+class ApartmentNode(DjangoObjectType):
     class Meta:
-        model = ApartmentProxy
+        model = Apartment
         max_complexity = 10
         filter_fields = {
             "street_address": ["exact"],
@@ -246,19 +87,19 @@ class ApartmentNode(IsTypeOfProxyPatch, DjangoObjectType):
         interfaces = (relay.Node,)
 
 
-class BuildingNode(IsTypeOfProxyPatch, DjangoObjectType):
+class BuildingNode(DjangoObjectType):
     apartments = DjangoConnectionField(ApartmentNode)
 
     class Meta:
-        model = BuildingProxy
+        model = Building
         interfaces = (relay.Node,)
 
 
-class RealEstateNode(IsTypeOfProxyPatch, DjangoObjectType):
+class RealEstateNode(DjangoObjectType):
     buildings = DjangoConnectionField(BuildingNode)
 
     class Meta:
-        model = RealEstateProxy
+        model = RealEstate
         interfaces = (relay.Node,)
 
 
@@ -300,210 +141,10 @@ class HousingCompanyFilterSet(FilterSet):
         ).filter(_address__icontains=value)
 
 
-class HousingCompanyNode(IsTypeOfProxyPatch, DjangoObjectType):
+class HousingCompanyNode(DjangoObjectType):
     real_estates = DjangoConnectionField(RealEstateNode)
 
     class Meta:
-        model = HousingCompanyProxy
+        model = HousingCompany
         interfaces = (relay.Node,)
         filterset_class = HousingCompanyFilterSet
-
-
-# Union
-
-
-class People(graphene.Union):
-    class Meta:
-        types = (
-            DeveloperType,
-            PropertyManagerType,
-            OwnerType,
-        )
-
-    @classmethod
-    def resolve_type(cls, instance: Model, info: GQLInfo) -> type[DjangoObjectType]:
-        if isinstance(instance, Developer):
-            return DeveloperType
-        if isinstance(instance, PropertyManager):
-            return PropertyManagerType
-        if isinstance(instance, Owner):
-            return OwnerType
-        msg = f"Unknown type: {instance}"
-        raise TypeError(msg)
-
-
-# --------------------------------------------------------------------
-
-
-class ExampleType(DjangoObjectType):
-    foo = graphene.String()
-
-    class Meta:
-        model = Example
-        fields = "__all__"
-
-    @required_annotations(foo=F("forward_one_to_one_field__name"))
-    def resolve_foo(self, info):
-        return self.foo
-
-
-class ForwardOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ForwardOneToOne
-        fields = "__all__"
-
-
-class ForwardManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ForwardManyToOne
-        fields = "__all__"
-
-
-class ForwardManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ForwardManyToMany
-        fields = "__all__"
-
-
-class ReverseOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOne
-        fields = "__all__"
-
-
-class ReverseOneToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToMany
-        fields = "__all__"
-
-
-class ReverseManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToMany
-        fields = "__all__"
-
-
-class ForwardOneToOneForRelatedType(DjangoObjectType):
-    class Meta:
-        model = ForwardOneToOneForRelated
-        fields = "__all__"
-
-
-class ForwardManyToOneForRelatedType(DjangoObjectType):
-    class Meta:
-        model = ForwardManyToOneForRelated
-        fields = "__all__"
-
-
-class ForwardManyToManyForRelatedType(DjangoObjectType):
-    class Meta:
-        model = ForwardManyToManyForRelated
-        fields = "__all__"
-
-
-class ReverseOneToOneToForwardOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOneToForwardOneToOne
-        fields = "__all__"
-
-
-class ReverseOneToOneToForwardManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOneToForwardManyToOne
-        fields = "__all__"
-
-
-class ReverseOneToOneToForwardManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOneToForwardManyToMany
-        fields = "__all__"
-
-
-class ReverseOneToOneToReverseOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOneToReverseOneToOne
-        fields = "__all__"
-
-
-class ReverseOneToOneToReverseManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOneToReverseOneToMany
-        fields = "__all__"
-
-
-class ReverseOneToOneToReverseManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToOneToReverseManyToMany
-        fields = "__all__"
-
-
-class ReverseOneToManyToForwardOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToManyToForwardOneToOne
-        fields = "__all__"
-
-
-class ReverseOneToManyToForwardManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToManyToForwardManyToOne
-        fields = "__all__"
-
-
-class ReverseOneToManyToForwardManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToManyToForwardManyToMany
-        fields = "__all__"
-
-
-class ReverseOneToManyToReverseOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToManyToReverseOneToOne
-        fields = "__all__"
-
-
-class ReverseOneToManyToReverseManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToManyToReverseOneToMany
-        fields = "__all__"
-
-
-class ReverseOneToManyToReverseManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseOneToManyToReverseManyToMany
-        fields = "__all__"
-
-
-class ReverseManyToManyToForwardOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToManyToForwardOneToOne
-        fields = "__all__"
-
-
-class ReverseManyToManyToForwardManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToManyToForwardManyToOne
-        fields = "__all__"
-
-
-class ReverseManyToManyToForwardManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToManyToForwardManyToMany
-        fields = "__all__"
-
-
-class ReverseManyToManyToReverseOneToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToManyToReverseOneToOne
-        fields = "__all__"
-
-
-class ReverseManyToManyToReverseManyToOneType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToManyToReverseOneToMany
-        fields = "__all__"
-
-
-class ReverseManyToManyToReverseManyToManyType(DjangoObjectType):
-    class Meta:
-        model = ReverseManyToManyToReverseManyToMany
-        fields = "__all__"
