5a2aa287498345713dda41e0ddcd2cc8893e294b
Rahul Somani <rsomani95@gmail.com>
2024-03-07 13:50:35 -0500

    blah

--- tests/example/types.py
+++ tests/example/types.py
@@ -53,12 +53,10 @@ from tests.example.models import (
     ReverseOneToOneToReverseOneToMany,
     ReverseOneToOneToReverseOneToOne,
     Sale,
-    TaggedItem,
-    TaggedItemDefaultUUID,
-    TaggableManyToManyRelatedField,
-    CustomTag,
+    DeveloperProxy,
     SegmentProperTags,
-    VideoAsset,
+    TaggedItemDefaultUUID,
+    TaggedItem,
 )
 
 __all__ = [
@@ -114,6 +112,260 @@ class PropertyManagerType(DjangoObjectType):
             "housing_companies",
         ]
 
+
+class HousingCompanyType(DjangoObjectType):
+    class Meta:
+        model = HousingCompany
+        fields = [
+            "pk",
+            "name",
+            "street_address",
+            "postal_code",
+            "city",
+            "developers",
+            "property_manager",
+            "real_estates",
+        ]
+
+    def resolve_name(model: HousingCompany, info: GQLInfo) -> str:
+        return model.name
+
+    greeting = graphene.String()
+    manager = graphene.String()
+    primary = graphene.String()
+
+    @required_fields("name")
+    def resolve_greeting(model: HousingCompany, info: GQLInfo) -> str:
+        return f"Hello {model.name}!"
+
+    @required_fields("property_manager__name")
+    def resolve_manager(model: HousingCompany, info: GQLInfo) -> str:
+        return model.property_manager.name
+
+    @required_fields("real_estates__name")
+    def resolve_primary(model: HousingCompany, info: GQLInfo) -> str:
+        return model.real_estates.first().name
+
+
+class RealEstateType(DjangoObjectType):
+    class Meta:
+        model = RealEstate
+        field = [
+            "pk",
+            "name",
+            "housing_company",
+            "buildings",
+        ]
+
+
+class BuildingType(DjangoObjectType):
+    class Meta:
+        model = Building
+        fields = [
+            "pk",
+            "name",
+            "street_address",
+            "real_estate",
+            "apartments",
+        ]
+
+
+class ApartmentType(DjangoObjectType):
+    class Meta:
+        model = Apartment
+        fields = [
+            "pk",
+            "completion_date",
+            "street_address",
+            "stair",
+            "floor",
+            "apartment_number",
+            "shares_start",
+            "shares_end",
+            "surface_area",
+            "rooms",
+            "building",
+            "sales",
+        ]
+        max_complexity = 10
+
+    @classmethod
+    def filter_queryset(cls, queryset: QuerySet, info: GQLInfo) -> QuerySet:
+        return queryset.filter(rooms__isnull=False)
+
+
+class SaleType(DjangoObjectType):
+    class Meta:
+        model = Sale
+        fields = [
+            "pk",
+            "apartment",
+            "purchase_price",
+            "purchase_date",
+            "ownerships",
+        ]
+
+    @classmethod
+    def filter_queryset(cls, queryset: QuerySet, info: GQLInfo) -> QuerySet:
+        return queryset.filter(purchase_price__gte=1)
+
+
+class OwnerType(DjangoObjectType):
+    class Meta:
+        model = Owner
+        fields = [
+            "pk",
+            "name",
+            "email",
+            "sales",
+            "ownerships",
+        ]
+
+
+class OwnershipType(DjangoObjectType):
+    class Meta:
+        model = Ownership
+        fields = [
+            "pk",
+            "owner",
+            "sale",
+            "percentage",
+        ]
+
+
+# Relay / Django-filters
+
+
+class CustomConnection(Connection):
+    class Meta:
+        abstract = True
+
+    total_count = graphene.Int()
+    edge_count = graphene.Int()
+
+    def resolve_total_count(root: Any, info: GQLInfo, **kwargs: Any) -> int:
+        return root.length
+
+    def resolve_edge_count(root: Any, info: GQLInfo, **kwargs: Any) -> int:
+        return len(root.edges)
+
+
+class IsTypeOfProxyPatch:
+    @classmethod
+    def is_type_of(cls, root: Any, info: GQLInfo) -> bool:
+        if cls._meta.model._meta.proxy:
+            return root._meta.model._meta.concrete_model == cls._meta.model._meta.concrete_model
+        return super().is_type_of(root, info)
+
+
+class DeveloperNode(IsTypeOfProxyPatch, DjangoObjectType):
+    class Meta:
+        model = DeveloperProxy
+        interfaces = (relay.Node,)
+        connection_class = CustomConnection
+
+
+class ApartmentNode(IsTypeOfProxyPatch, DjangoObjectType):
+    sales = DjangoListField(SaleType)
+
+    class Meta:
+        model = ApartmentProxy
+        max_complexity = 10
+        connection_class = CustomConnection
+        filter_fields = {
+            "street_address": ["exact"],
+            "building__name": ["exact"],
+        }
+        interfaces = (relay.Node,)
+
+
+class BuildingFilterSet(FilterSet):
+    order_by = OrderingFilter(fields=["name"])
+
+    class Meta:
+        model = Building
+        fields = ["name", "street_address"]
+
+
+class BuildingNode(IsTypeOfProxyPatch, DjangoObjectType):
+    apartments = DjangoConnectionField(ApartmentNode)
+
+    class Meta:
+        model = BuildingProxy
+        interfaces = (relay.Node,)
+        filterset_class = BuildingFilterSet
+
+
+class RealEstateNode(IsTypeOfProxyPatch, DjangoObjectType):
+    building_set = DjangoConnectionField(BuildingNode)
+
+    class Meta:
+        model = RealEstateProxy
+        interfaces = (relay.Node,)
+
+
+class HousingCompanyFilterSet(FilterSet):
+    order_by = OrderingFilter(
+        fields=[
+            "name",
+            "street_address",
+            "postal_code__code",
+            "city",
+            "developers__name",
+        ],
+    )
+
+    address = CharFilter(method="filter_address")
+
+    class Meta:
+        model = HousingCompany
+        fields = {
+            "name": ["iexact", "icontains"],
+            "street_address": ["iexact", "icontains"],
+            "postal_code__code": ["iexact"],
+            "city": ["iexact", "icontains"],
+            "developers__name": ["iexact", "icontains"],
+        }
+
+    def filter_address(self, qs: QuerySet[HousingCompany], name: str, value: str) -> QuerySet[HousingCompany]:
+        return qs.alias(
+            _address=Concat(
+                F("street_address"),
+                Value(", "),
+                F("postal_code__code"),
+                Value(" "),
+                F("city"),
+            ),
+        ).filter(_address__icontains=value)
+
+
+class HousingCompanyNode(IsTypeOfProxyPatch, DjangoObjectType):
+    real_estates = DjangoConnectionField(RealEstateNode)
+    developers = DjangoConnectionField(DeveloperNode)
+
+    class Meta:
+        model = HousingCompanyProxy
+        interfaces = (relay.Node,)
+        connection_class = CustomConnection
+        filterset_class = HousingCompanyFilterSet
+
+
+class PropertyManagerFilterSet(FilterSet):
+    order_by = OrderingFilter(fields=["name"])
+
+    class Meta:
+        model = PropertyManager
+        fields = ["name", "email"]
+
+
+class PropertyManagerNode(IsTypeOfProxyPatch, DjangoObjectType):
+    housing_companies = DjangoConnectionField(HousingCompanyNode)
+
+    class Meta:
+        model = PropertyManagerProxy
+        interfaces = (relay.Node,)
+        connection_class = CustomConnection
+        filterset_class = PropertyManagerFilterSet
 
 
 from django.db import models
@@ -271,301 +523,6 @@ class VideoAssetNode(DjangoObjectType):
     class Meta:
         model = VideoAsset
         interfaces = (relay.Node,)
-
-
-class HousingCompanyType(DjangoObjectType):
-    class Meta:
-        model = HousingCompany
-        fields = [
-            "pk",
-            "name",
-            "street_address",
-            "postal_code",
-            "city",
-            "developers",
-            "property_manager",
-            "real_estates",
-        ]
-
-    def resolve_name(model: HousingCompany, info: GQLInfo) -> str:
-        return model.name
-
-    greeting = graphene.String()
-    manager = graphene.String()
-    primary = graphene.String()
-
-    tagged_items = DjangoListField(TaggedItemType)
-    custom_tags = DjangoListField(CustomTagType)
-
-    # def resolve_custom_tags(model: HousingCompany, info: GQLInfo):
-    #     logger.debug("Custom tageditem resolver")
-    #     return TaggedItem.objects.filter(object_id=model.id)
-
-    # custom_tags = graphene.List(CustomTagType)
-
-    # def resolve_custom_tags(model: HousingCompany, info: GQLInfo):
-    #     return CustomTag.objects.filter(object_id=model.id)
-
-    # def resolve_tagged_items(model: HousingCompany, info: GQLInfo):
-    #     return TaggedItem.objects.filter(object_id=model.id)
-
-    @required_fields("name")
-    def resolve_greeting(model: HousingCompany, info: GQLInfo) -> str:
-        return f"Hello {model.name}!"
-
-    @required_fields("property_manager__name")
-    def resolve_manager(model: HousingCompany, info: GQLInfo) -> str:
-        return model.property_manager.name
-
-    @required_fields("real_estates__name")
-    def resolve_primary(model: HousingCompany, info: GQLInfo) -> str:
-        return model.real_estates.first().name
-
-
-class RealEstateType(DjangoObjectType):
-    class Meta:
-        model = RealEstate
-        field = [
-            "pk",
-            "name",
-            "housing_company",
-            "buildings",
-        ]
-
-
-class BuildingType(DjangoObjectType):
-    class Meta:
-        model = Building
-        fields = [
-            "pk",
-            "name",
-            "street_address",
-            "real_estate",
-            "apartments",
-        ]
-
-
-class ApartmentType(DjangoObjectType):
-    class Meta:
-        model = Apartment
-        fields = [
-            "pk",
-            "completion_date",
-            "street_address",
-            "stair",
-            "floor",
-            "apartment_number",
-            "shares_start",
-            "shares_end",
-            "surface_area",
-            "rooms",
-            "building",
-            "sales",
-        ]
-        max_complexity = 10
-
-    @classmethod
-    def filter_queryset(cls, queryset: QuerySet, info: GQLInfo) -> QuerySet:
-        return queryset.filter(rooms__isnull=False)
-
-
-class SaleType(DjangoObjectType):
-    class Meta:
-        model = Sale
-        fields = [
-            "pk",
-            "apartment",
-            "purchase_price",
-            "purchase_date",
-            "ownerships",
-        ]
-
-    @classmethod
-    def filter_queryset(cls, queryset: QuerySet, info: GQLInfo) -> QuerySet:
-        return queryset.filter(purchase_price__gte=1)
-
-
-class OwnerType(DjangoObjectType):
-    class Meta:
-        model = Owner
-        fields = [
-            "pk",
-            "name",
-            "email",
-            "sales",
-            "ownerships",
-        ]
-
-
-class OwnershipType(DjangoObjectType):
-    class Meta:
-        model = Ownership
-        fields = [
-            "pk",
-            "owner",
-            "sale",
-            "percentage",
-        ]
-
-
-# Relay / Django-filters
-
-
-class CustomConnection(Connection):
-    class Meta:
-        abstract = True
-
-    total_count = graphene.Int()
-    edge_count = graphene.Int()
-
-    def resolve_total_count(root: Any, info: GQLInfo, **kwargs: Any) -> int:
-        return root.length
-
-    def resolve_edge_count(root: Any, info: GQLInfo, **kwargs: Any) -> int:
-        return len(root.edges)
-
-
-class IsTypeOfProxyPatch:
-    @classmethod
-    def is_type_of(cls, root: Any, info: GQLInfo) -> bool:
-        if cls._meta.model._meta.proxy:
-            return root._meta.model._meta.concrete_model == cls._meta.model._meta.concrete_model
-        return super().is_type_of(root, info)
-
-
-class DeveloperNode(IsTypeOfProxyPatch, DjangoObjectType):
-    class Meta:
-        model = DeveloperProxy
-        interfaces = (relay.Node,)
-        connection_class = CustomConnection
-
-
-class ApartmentNode(IsTypeOfProxyPatch, DjangoObjectType):
-    sales = DjangoListField(SaleType)
-
-    class Meta:
-        model = ApartmentProxy
-        max_complexity = 10
-        connection_class = CustomConnection
-        filter_fields = {
-            "street_address": ["exact"],
-            "building__name": ["exact"],
-        }
-        interfaces = (relay.Node,)
-
-
-class BuildingFilterSet(FilterSet):
-    # order_by = OrderingFilter(fields=["name"])
-
-    class Meta:
-        model = Building
-        fields = ["name", "street_address"]
-
-
-class BuildingNode(IsTypeOfProxyPatch, DjangoObjectType):
-    apartments = DjangoConnectionField(ApartmentNode)
-
-    class Meta:
-        model = BuildingProxy
-        interfaces = (relay.Node,)
-        filterset_class = BuildingFilterSet
-
-
-class RealEstateNode(IsTypeOfProxyPatch, DjangoObjectType):
-    building_set = DjangoConnectionField(BuildingNode)
-
-    class Meta:
-        model = RealEstateProxy
-        interfaces = (relay.Node,)
-        connection_class = CustomConnection
-
-
-class HousingCompanyFilterSet(FilterSet):
-    # order_by = OrderingFilter(
-    #     fields=[
-    #         "name",
-    #         "street_address",
-    #         "postal_code__code",
-    #         "city",
-    #         "developers__name",
-    #     ],
-    # )
-
-    address = CharFilter(method="filter_address")
-
-    class Meta:
-        model = HousingCompany
-        fields = {
-            "name": ["iexact", "icontains"],
-            "street_address": ["iexact", "icontains"],
-            "postal_code__code": ["iexact"],
-            "city": ["iexact", "icontains"],
-            "developers__name": ["iexact", "icontains"],
-        }
-
-    def filter_address(self, qs: QuerySet[HousingCompany], name: str, value: str) -> QuerySet[HousingCompany]:
-        return qs.alias(
-            _address=Concat(
-                F("street_address"),
-                Value(", "),
-                F("postal_code__code"),
-                Value(" "),
-                F("city"),
-            ),
-        ).filter(_address__icontains=value)
-
-
-class DeveloperNode(DjangoObjectType):
-
-    class Meta:
-        model = Developer
-        interfaces = (relay.Node,)
-
-
-class HousingCompanyNode(IsTypeOfProxyPatch, DjangoObjectType):
-    real_estates = DjangoConnectionField(RealEstateNode)
-    developers = DjangoConnectionField(DeveloperNode)
-    tagged_items = DjangoListField(TaggedItemType)
-
-
-    ozu_tags = DjangoListField(TaggedItemUUIDType)
-    @required_fields("tagged_items")
-    def resolve_ozu_tags(model: SegmentProperTags, info: GQLInfo):
-        return TaggedItemDefaultUUID.objects.filter(object_id=model.id)
-
-    # custom_tags = DjangoListField(CustomTagType)
-
-    # def resolve_tagged_items(model: HousingCompany, info: GQLInfo):
-    #     # logger.info("Custom tageditem resolver")
-    #     return TaggedItem.objects.filter(object_id=model.id)
-
-    class Meta:
-        model = HousingCompanyProxy
-        interfaces = (relay.Node,)
-        connection_class = CustomConnection
-        filterset_class = HousingCompanyFilterSet
-        connection_class = CustomConnection
-
-
-class PropertyManagerFilterSet(FilterSet):
-    # order_by = OrderingFilter(fields=["name"])
-
-    class Meta:
-        model = PropertyManager
-        fields = ["name", "email"]
-        connection_class = CustomConnection
-
-
-class PropertyManagerNode(IsTypeOfProxyPatch, DjangoObjectType):
-    housing_companies = DjangoConnectionField(HousingCompanyNode)
-
-    class Meta:
-        model = PropertyManagerProxy
-        interfaces = (relay.Node,)
-        connection_class = CustomConnection
-        filterset_class = PropertyManagerFilterSet
-        connection_class = CustomConnection
-
 
 # Union
 
